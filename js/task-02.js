98dfec0871b6f5479532f1bc24dc4bd66a310379
8wondrs <8wondrs@gmail.com>
2023-03-18 12:13:07 +0200

    add task-02 - ✅

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,10 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const listEl = document.querySelector('#ingredients');
+
+const createIngredientsList = ingredients.map(ingredient => {
+  const items = document.createElement('li');
+  items.textContent = ingredient;
+  items.classList.add('item');
+  listEl.append(items);
+});
