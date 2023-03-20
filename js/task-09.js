05ffeac8b7e026b03818a920a76a25d3f0ae22ae
8wondrs <8wondrs@gmail.com>
2023-03-20 17:07:36 +0200

    add task-09 - ✅

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const spanEl = document.querySelector('.color');
+const changeColor = document.querySelector('.change-color');
+
+changeColor.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  body.setAttribute(
+    'style',
+    `background-color: ${randomColor}; transition: background-color 500ms linear`
+  );
+  spanEl.textContent = `${randomColor}`;
+});
