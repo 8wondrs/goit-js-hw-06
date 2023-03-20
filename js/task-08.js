ff826de0bb5023db3cdb1a512dba9b6bab2ac2b1
8wondrs <8wondrs@gmail.com>
2023-03-20 16:19:59 +0200

    add task-08 - ✅

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener('submit', event => {
+  event.preventDefault();
+  const emailInput = loginForm.email.value;
+  const passwordInput = loginForm.password.value;
+
+  if (emailInput === '' || passwordInput === '') {
+    alert('Будь ласка, заповніть всі поля');
+  } else {
+    const formData = {
+      email: emailInput,
+      password: passwordInput,
+    };
+    console.log(formData);
+    loginForm.reset();
+  }
+});
